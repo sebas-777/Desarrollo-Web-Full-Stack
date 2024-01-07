b3ddfb480bf366433beb681973aa62546359ee6c
Sebastian Castro <scastro@qvision.com.co>
2024-01-06 20:59:53 -0500

    useState

--- part1/part1/src/Components/Welcome.jsx
+++ part1/part1/src/Components/Welcome.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Welcome(props){
     
@@ -6,13 +6,28 @@ export default function Welcome(props){
 
     const[ counter,setCounter] = useState(0);
     // eslint-disable-next-line react/prop-types
+    const [semaforo, setsemaforo] = useState(false)
+     // eslint-disable-next-line react/prop-types
     const {message,name} = props;
+
+    const contar =()=>{
+        console.log('Entrando en la funcion de contar ');
+        setCounter(counter + 1);
+        setsemaforo(!semaforo)
+    }; 
+    console.log(semaforo)
+
+    useEffect(() =>{
+        console.log(semaforo);
+    },[semaforo])
+
     return (
         <div>
             <h1> Hola ,{name}</h1>
             <h2>Contador de React con Hooks</h2>
             <h4>El numero de Contador es {counter}</h4>
-            <button type="submit" onClick={() => setCounter(counter+1)}>
+            <button type="submit" onClick={contar}>
+            <p> el semaforo esta en color {semaforo ? 'red' : 'green'}</p>
                 Sumar contador
             </button>
             <p>{message}</p>
