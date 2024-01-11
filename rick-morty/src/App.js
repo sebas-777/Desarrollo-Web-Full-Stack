028a3ccc44147081c948bb17dfcea743ebc238ef
Sebastian Castro <scastro@qvision.com.co>
2024-01-10 23:59:29 -0500

    aplicanco ternarios,, enlace de volver home

--- rick-morty/src/App.js
+++ rick-morty/src/App.js
@@ -1,6 +1,7 @@
 import imageRickMorty from './img/rick-morty.png';
 import './App.css';
 import { useState } from 'react';
+import Characters from './components/Characters';
 
 function App() { 
   const[characters, setCharacters] = useState(null);
@@ -13,15 +14,20 @@ function App() {
      
   } 
 
-  console.log(characters)
-
   return (
     <div className="App">
       <header className="App-header">
         <h1 className='title'>Rick & Morty</h1>
-        <img src={imageRickMorty} alt="Rick & Morty" className='img-home'/>
-        <button onClick={reqApi} className='btn-search'>Buscar Personajes</button>
-       
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : ( 
+          <>
+          <img src={imageRickMorty} alt="Rick & Morty" className='img-home'/>
+          <button onClick={reqApi} className='btn-search'>
+          Buscar Personajes
+          </button>
+          </>
+        )}
       </header>
     </div>
   );
